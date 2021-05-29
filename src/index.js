fda4da669f38b4a0d7ce9a185122173daa0e112f
Beatriz Oliveira <anabeatrizxoliveira@gmail.com>
2021-05-29 18:58:20 -0300

    feat(koa-router): add koa-router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 // hello world koa
 
 const Koa = require('koa');
+const Router = require('@koa/router');
+
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => {
-    ctx.body = 'Hello World';
-});
+router.get('koa-server', '/', (ctx) => {
+    ctx.body = 'Hello World'
+})
 
-app.listen(3000);
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
+
+app.listen(8080);
