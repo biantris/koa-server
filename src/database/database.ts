b95fd16115d8f8aa310c0b6b650cd2857c140f36
beatrizoliveiira <anabeatrizxoliveira@gmail.com>
2022-04-26 14:45:24 -0300

    feat(database): add connect database

---
+++ src/database/database.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = () => {
+  mongoose.connect(`mongodb://localhost/graphql-crud`, {
+    useNewUrlParser: true,
+  });
+
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", () => console.log("Database connected."));
+};
+
+export default connectDB;
