55c47b0633205aa98aec3ff21e99eb5c52119773
beatrizoliveiira <anabeatrizxoliveira@gmail.com>
2022-05-06 21:41:21 -0300

    feat(event-type): add eventType

---
+++ src/modules/event/EventType.ts
@@ -0,0 +1,43 @@
+import { GraphQLBoolean, GraphQLObjectType, GraphQLString } from "graphql";
+
+import { globalIdField } from "graphql-relay";
+
+import { connectionDefinitions } from "@entria/graphql-mongo-helpers";
+
+import { load } from "./EventLoader";
+
+import { nodeInterface, registerTypeLoader } from "../node/typeRegister";
+
+const EventType = new GraphQLObjectType({
+  name: "Event",
+  description: "event data",
+  fields: () => ({
+    id: globalIdField("Event"),
+    name: {
+      type: GraphQLString,
+      resolve: event => event.name,
+    },
+    start: {
+        type: GraphQLString,
+        resolve: event => event.start,
+    },
+    end: {
+        type: GraphQLString,
+        resolve: event => event.end,
+    },
+    allDay: {
+        type: GraphQLBoolean,
+        resolve: event => event.allDay,
+    },
+  }),
+  interfaces: () => [nodeInterface],
+});
+
+registerTypeLoader(EventType, load);
+
+export default EventType;
+
+export const EventConnection = connectionDefinitions({
+  name: "Event",
+  nodeType: EventType,
+});
