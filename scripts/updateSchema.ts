33150a1e1de7b0abfa2a3ed222da4c43f53a27c8
beatrizoliveiira <anabeatrizxoliveira@gmail.com>
2022-05-14 16:57:54 -0300

    feat(up-schema): add update schema

---
+++ scripts/updateSchema.ts
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+import { promisify } from 'util';
+
+import { printSchema } from 'graphql/utilities';
+
+import { schema as schemaGraphql } from '../src/schema/schema';
+
+const writeFileAsync = promisify(fs.writeFile);
+
+const cwd = process.cwd();
+
+(async () => {
+  const configs = [
+    {
+      schema: schemaGraphql,
+      path: path.join(cwd, `./schema.graphql`),
+    },
+  ];
+
+  await Promise.all([
+    ...configs.map(async config => {
+      await writeFileAsync(config.path, printSchema(config.schema));
+    }),
+  ]);
+
+  process.exit(0);
+})();
