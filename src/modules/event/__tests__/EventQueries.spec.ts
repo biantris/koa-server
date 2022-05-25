512a243d7ca8ba7feaec9cab799cd903aa1d0d6e
beatrizoliveiira <anabeatrizxoliveira@gmail.com>
2022-05-25 19:40:41 -0300

    feat(event-test): fix collection

--- src/modules/event/__tests__/EventQueries.spec.ts
+++ src/modules/event/__tests__/EventQueries.spec.ts
@@ -1,15 +1,10 @@
-import { graphql } from "graphql";
+import { graphql } from 'graphql';
 
-import {
-  clearDbAndRestartCounters,
-  connectMongoose,
-  disconnectMongoose,
-  sanitizeTestObject,
-} from "../../../../test";
+import { clearDbAndRestartCounters, connectMongoose, disconnectMongoose, sanitizeTestObject } from '../../../../test';
 
-import { schema } from "../../../schema/schema";
+import { schema } from '../../../schema/schema';
 
-import { createEvent } from "../fixture/createEvent";
+import { createEvent } from '../fixture/createEvent';
 
 beforeAll(connectMongoose);
 
@@ -17,18 +12,18 @@ beforeEach(clearDbAndRestartCounters);
 
 afterAll(disconnectMongoose);
 
-it("should query all events", async () => {
+it('should query all events', async () => {
   const event = await createEvent({
-    name: "event",
-    start: "2019-01-01T00:00:00.000Z",
-    end: "2019-01-01T23:59:59.000Z",
+    name: 'event A',
+    start: '2019-01-01T00:00:00.000Z',
+    end: '2019-01-01T23:59:59.000Z',
     allDay: true,
   });
 
   const eventB = await createEvent({
-    name: "eventB",
-    start: "2019-01-02T00:00:00.000Z",
-    end: "2019-01-02T23:59:59.000Z",
+    name: 'event B',
+    start: '2019-01-02T00:00:00.000Z',
+    end: '2019-01-02T23:59:59.000Z',
     allDay: false,
   });
 
@@ -50,25 +45,19 @@ it("should query all events", async () => {
 
   const rootValue = {};
   //const contextValue = await getContext({});
-  const variableValues = {};
+  const variables = {};
 
-  const result = await graphql({
-    schema,
-    source: query,
-    rootValue,
-    //contextValue,
-    variableValues,
-  });
+  const result = await graphql(schema, query, rootValue, variables);
 
   expect(result.errors).toBeUndefined();
 
   // eslint-disable-next-line
-  console.log("result: ", result.data.events);
+  console.log('result: ', result.data.events.node.name);
 
   expect(result.data.events.edges.length).toBe(2);
 
-  expect(result.data.events.edges[0].node.name).toBe(event.name);
-  expect(result.data.events.edges[1].node.name).toBe(eventB.name);
+  expect(result.data.events.edges.node.name).toBe(event.name);
+  expect(result.data.events.edges.node.name).toBe(eventB.name);
 
   expect(sanitizeTestObject(result.data)).toMatchSnapshot();
 });
