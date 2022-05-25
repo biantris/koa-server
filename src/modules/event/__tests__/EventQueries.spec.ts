829b3931fe577550f0cb5b9e16e9e76eb29a08ae
beatrizoliveiira <anabeatrizxoliveira@gmail.com>
2022-05-25 15:54:25 -0300

    feat(event-test): fix event test

--- src/modules/event/__tests__/EventQueries.spec.ts
+++ src/modules/event/__tests__/EventQueries.spec.ts
@@ -18,8 +18,19 @@ beforeEach(clearDbAndRestartCounters);
 afterAll(disconnectMongoose);
 
 it("should query all events", async () => {
-  const event = await createEvent();
-  const anotherEvent = await createEvent();
+  const event = await createEvent({
+    name: "Event",
+    start: "2019-01-01T00:00:00.000Z",
+    end: "2019-01-01T23:59:59.000Z",
+    allDay: true,
+  });
+
+  const anotherEvent = await createEvent({
+    name: "Another Event",
+    start: "2019-01-02T00:00:00.000Z",
+    end: "2019-01-02T23:59:59.000Z",
+    allDay: false,
+  });
 
   // language=GraphQL
   const query = `
@@ -50,6 +61,11 @@ it("should query all events", async () => {
 
   // eslint-disable-next-line
   console.log("result: ", result.data.events);
+
   expect(result.data.events.edges.length).toBe(2);
+
+  expect(result.data.events.edges[0].node.name).toBe(event.name);
+  expect(result.data.events.edges[1].node.name).toBe(anotherEvent.name);
+
   expect(sanitizeTestObject(result.data)).toMatchSnapshot();
 });
