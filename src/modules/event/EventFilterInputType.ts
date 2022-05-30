04e098dd37ae1823c13eab1f4b0256f90b3eb634
beatrizoliveiira <anabeatrizxoliveira@gmail.com>
2022-05-30 12:06:29 -0300

    feat(fix-filter): fix event filter

--- src/modules/event/EventFilterInputType.ts
+++ src/modules/event/EventFilterInputType.ts
@@ -5,11 +5,7 @@ import { FILTER_CONDITION_TYPE } from '@entria/graphql-mongo-helpers';
 import { getObjectId } from "@entria/graphql-mongo-helpers";
 
 export const eventFilterMapping = {
-  user: {
-    type: FILTER_CONDITION_TYPE.MATCH_1_TO_1,
-    format: (val: string) => val && getObjectId(val),
-  },
-  post: {
+  name: {
     type: FILTER_CONDITION_TYPE.MATCH_1_TO_1,
     format: (val: string) => val && getObjectId(val),
   },
